362b7a44336d7a61916cf3e9892bc366a721facf
Sashank <144649279+sashank-sai@users.noreply.github.com>
2025-07-06 15:52:05 +0530

    updted main init

--- app.py
+++ app.py
@@ -126,4 +126,6 @@ def send_uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render will pass the PORT
+    app.run(host='0.0.0.0', port=port) 
